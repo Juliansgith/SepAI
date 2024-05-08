5547b69d224f1bf0ae4db0489542750083286734
Julian Cooper <115541810+Juliansgith@users.noreply.github.com>
2024-05-08 18:21:42 +0200

    removed aibrain, seems obsolete

--- hook/lua/aibrains/index.lua
+++ hook/lua/aibrains/index.lua
@@ -3,3 +3,4 @@ local NewAIBrain = import('/mods/SepAI/lua/AI/SepBrain.lua')
 keyToBrain = keyToBrain or {}
 keyToBrain["sepai"] = SepBrain.NewAIBrain
 keyToBrain["sepaicheat"] = SepBrain.NewAIBrain
+sa
