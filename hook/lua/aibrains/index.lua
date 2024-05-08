61e404faf83e12c65b932eb4f340f700658e4b8f
Julian Cooper <115541810+Juliansgith@users.noreply.github.com>
2024-05-08 18:22:11 +0200

    removed typo

--- hook/lua/aibrains/index.lua
+++ hook/lua/aibrains/index.lua
@@ -3,4 +3,3 @@ local NewAIBrain = import('/mods/SepAI/lua/AI/SepBrain.lua')
 keyToBrain = keyToBrain or {}
 keyToBrain["sepai"] = SepBrain.NewAIBrain
 keyToBrain["sepaicheat"] = SepBrain.NewAIBrain
-sa
